168c3cbb11bd45617f1015612c46bfa851bc863f
AdamRhoades95 <74215378+AdamRhoades95@users.noreply.github.com>
2021-07-27 10:25:47 -0400

    Add files via upload

---
+++ KimsProject/public_html/js/test1.js
@@ -0,0 +1,22 @@
+
+
+
+// Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+    apiKey: "AIzaSyBKpk7-U_TkE3IApTOZFSMNIXuOg-PYPFs",
+    authDomain: "kims-streamer.firebaseapp.com",
+    databaseURL: "https://kims-streamer-default-rtdb.firebaseio.com",
+    projectId: "kims-streamer",
+    storageBucket: "kims-streamer.appspot.com",
+    messagingSenderId: "1059071438295",
+    appId: "1:1059071438295:web:bd5fe41d405af2e161ef92",
+    measurementId: "G-13T8LXKL81"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  
+  $("#login").click(function(){
+      
+  });
+  
